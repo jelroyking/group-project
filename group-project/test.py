7d2776c96ca6594988585da520a84bed3240d30d
jake_robinson1998 <jake_robinson1998@yahoo.co.uk>
2016-11-18 11:31:42 -0500

    new random change

---
+++ group-project/test.py
@@ -0,0 +1,72 @@
+#Random comment
+import csv
+out=open("DataExtraction.csv","rt")
+data=csv.reader(out)
+data=[row for row in data]
+
+date=[row[0] for row in data]
+data_steps=[row[1] for row in data]
+data_calories=[row[2] for row in data]
+data_kilo=[row[6] for row in data]
+del date[0]
+del data_steps[0]
+del data_calories[0]
+del data_kilo[0]
+steps=[int(x) for x in data_steps]
+calories=[int(x) for x in data_calories]
+kilo=[float(x) for x in data_kilo]
+out.close()
+
+def averageSteps():
+    """A function to calculate the average number of steps
+    taken each day using the data extracted from the csv file"""
+    totalSteps=sum(steps)
+    listLength=len(steps)
+    averageSteps=totalSteps/listLength
+    print("You take an average of",averageSteps,"steps a day")
+    return(averageSteps)
+
+def averageCalories():
+    """A function to calculate the average number of calories
+    burned each day using the data extracted from the csv file"""
+    totalCalories=sum(calories)
+    listLength=len(calories)
+    averageCalories=totalCalories/listLength
+    print("You burn an average of",averageCalories,"calories a day")
+    return(averageCalories)
+
+def averageDistance():
+    """A function to calculate the average
+    distance travelled per day in kilometers"""
+    totalKilo=sum(kilo)
+    listLength=len(kilo)
+    averageKilo=round(totalKilo/listLength,2)
+    print("You travel an average of",averageKilo,"kilometers a day")
+    return(averageCalories)
+
+print("A: Steps")
+print("B: Calories")
+print("C: Distance")
+menuChoice=str(input("Select on of the above - "))
+if menuChoice == "A":
+  print("A: Total number of steps")
+  print("A: Total number of steps")
+  print("A: Total number of steps")
+elif menuChoice == "B":
+  print("A: ...")
+elif menuChoice == "C":
+  print("A: ...")
+else:
+  print("Please enter a valid menu option")
+
+
+
+
+
+
+
+
+
+
+
+
