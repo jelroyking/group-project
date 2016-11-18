a63fd1cdeb62e78c8eda0f51fb62588262ec6bdb
jake_robinson1998 <jake_robinson1998@yahoo.co.uk>
2016-11-18 12:06:09 -0500

    started to create the menus

--- group-project/test.py
+++ group-project/test.py
@@ -17,12 +17,28 @@ calories=[int(x) for x in data_calories]
 kilo=[float(x) for x in data_kilo]
 out.close()
 
+def totalSteps():
+    totalSteps=sum(steps)
+    print()
+    print("You have taken a total of ",totalSteps,"steps")
+    
+def totalCalories():
+    totalCalories=sum(calories)
+    print()
+    print("You have burned a total of ",totalCalories,"calories")
+    
+def totalDistance():
+    totalDistance=sum(distance)
+    print()
+    print("You have travelled a total of ",totalDistance,"kilometres")
+    
 def averageSteps():
     """A function to calculate the average number of steps
     taken each day using the data extracted from the csv file"""
     totalSteps=sum(steps)
     listLength=len(steps)
     averageSteps=totalSteps/listLength
+    print()
     print("You take an average of",averageSteps,"steps a day")
     return(averageSteps)
 
@@ -32,6 +48,7 @@ def averageCalories():
     totalCalories=sum(calories)
     listLength=len(calories)
     averageCalories=totalCalories/listLength
+    print()
     print("You burn an average of",averageCalories,"calories a day")
     return(averageCalories)
 
@@ -41,6 +58,7 @@ def averageDistance():
     totalKilo=sum(kilo)
     listLength=len(kilo)
     averageKilo=round(totalKilo/listLength,2)
+    print()
     print("You travel an average of",averageKilo,"kilometers a day")
     return(averageCalories)
 
@@ -49,13 +67,32 @@ print("B: Calories")
 print("C: Distance")
 menuChoice=str(input("Select on of the above - "))
 if menuChoice == "A":
+  print()
   print("A: Total number of steps")
-  print("A: Total number of steps")
-  print("A: Total number of steps")
+  print("B: Average number of steps")
+  stepsMenuChoice=str(input("Select on of the above - "))
+  if stepsMenuChoice == "A":
+    totalSteps()
+  elif stepsMenuChoice == "B":
+    averageSteps()
 elif menuChoice == "B":
-  print("A: ...")
+  print()
+  print("A: Total number of calories")
+  print("B: Average number of calories")
+  caloriesMenuChoice=str(input("Select on of the above - "))
+  if caloriesMenuChoice == "A":
+    totalCalories()
+  elif caloriesMenuChoice == "B":
+    averageCalories()
 elif menuChoice == "C":
-  print("A: ...")
+  print()
+  print("A: Total distance")
+  print("B: Average distance")
+  distanceMenuChoice=str(input("Select on of the above - "))
+  if distanceMenuChoice == "A":
+    totalDistance()
+  elif distanceMenuChoice == "B":
+    averageDistance()
 else:
   print("Please enter a valid menu option")
 
